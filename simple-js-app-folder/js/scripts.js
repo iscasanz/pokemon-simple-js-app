5e2517587149bf50918721dd4d44366bf360a8a7
iscasanz <155112885+iscasanz@users.noreply.github.com>
2024-03-25 10:54:55 -0700

    Add files via upload

--- simple-js-app-folder/js/scripts.js
+++ simple-js-app-folder/js/scripts.js
@@ -1,2 +1,6 @@
-alert('Hello world')
+let pokemonList = [{name: "squirtle", type:["water", "grass"]},
 
+{name: "evvee", type:["water", "speed"]},
+
+{name: "jigglypuff", type:["fairy", "poison"]}
+];
