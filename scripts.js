77a2a112e36d9aed2da75e39ae36993e0e9c2c9a
iscasanz <155112885+iscasanz@users.noreply.github.com>
2024-04-10 17:52:10 -0700

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,6 +1,31 @@
-[{name: "squirtle", type:["water", "grass"]},
+document.write("hello world");
 
-{name: "evvee", type:["water", "speed"]},
+let pokemonList = [
+  {
+    name: "squirtle",
+    height: 0.5,
+    types: ["water", "grass"],
+  },
 
-{name: "jigglypuff", type:["fairy", "poison"]}
+  {
+    name: "evvee",
+    height: 0.3,
+    types: ["water", "speed"],
+  },
+
+  {
+    name: "charmander",
+    height: 0.6,
+    types: ["fairy", "ice"],
+  },
 ];
+
+for (let i = 0; i < pokemonList.length; i++) {
+  if (pokemonList[i].height < 0.6 && pokemonList[i].height > 0.3) {
+    console.log(pokemonList[i].name + " is average");
+  } else if (pokemonList[i].height <= 0.3) {
+    console.log(pokemonList[i].name + " is small");
+  } else {
+    console.log(pokemonList[i].name + " is large");
+  }
+}
