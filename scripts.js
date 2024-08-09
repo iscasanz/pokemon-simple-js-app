5f29c4fe06f8b97107a328e50a18e761e88f81fd
iscasanz <155112885+iscasanz@users.noreply.github.com>
2024-08-09 10:19:30 -0700

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,31 +1,42 @@
-document.write("hello world");
+let pokemonRepository = (function () {
+  let pokemonList = [
+    {
+      name: "squirtle",
+      height: 0.5,
+      types: ["water", "grass"],
+    },
 
-let pokemonList = [
-  {
-    name: "squirtle",
-    height: 0.5,
-    types: ["water", "grass"],
-  },
+    {
+      name: "evvee",
+      height: 0.3,
+      types: ["water", "speed"],
+    },
 
-  {
-    name: "evvee",
-    height: 0.3,
-    types: ["water", "speed"],
-  },
+    {
+      name: "charmander",
+      height: 0.6,
+      types: ["fairy", "ice"],
+    },
+  ];
 
-  {
-    name: "charmander",
-    height: 0.6,
-    types: ["fairy", "ice"],
-  },
-];
+  pokemonList.forEach((item) => console.log(item));
 
-for (let i = 0; i < pokemonList.length; i++) {
-  if (pokemonList[i].height < 0.6 && pokemonList[i].height > 0.3) {
-    console.log(pokemonList[i].name + " is average");
-  } else if (pokemonList[i].height <= 0.3) {
-    console.log(pokemonList[i].name + " is small");
-  } else {
-    console.log(pokemonList[i].name + " is large");
+  function add(pokemon) {
+    pokemonList.push(pokemon);
   }
-}
+
+  function getAll() {
+    return pokemonList;
+  }
+
+  return {
+    add: add,
+    getAll: getAll,
+  };
+})();
+
+console.log(pokemonRepository.getAll()); // []
+pokemonRepository.add({ name: "test" });
+
+// to check if the pokemon was added to list or not
+console.log(pokemonRepository.getAll()); // []
